c66b5bc1b999a31c4e3c0823bbbf1461ba4fe0b1
Stephen Cooper <scooperdev@gmail.com>
2022-08-10 09:45:16 +0100

    add ag-grid

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,54 +1,44 @@
 <template>
-  <h1>{{ msg }}</h1>
-
-  <p>
-    Recommended IDE setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
-    +
-    <a
-      href="https://marketplace.visualstudio.com/items?itemName=octref.vetur"
-      target="_blank"
-    >
-      Vetur
-    </a>
-    or
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    (if using
-    <code>&lt;script setup&gt;</code>)
-  </p>
-
-  <p>See <code>README.md</code> for more information.</p>
-
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Docs
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
-  </p>
-
-  <button type="button" @click="count++">count is: {{ count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+  <ag-grid-vue
+    style="width: 90%; height: 500px"
+    class="ag-theme-alpine"
+    :column-defs="columnDefs"
+    :row-data="rowData"
+    :enable-charts="true"
+    :enable-range-selection="true"
+  >
+  </ag-grid-vue>
 </template>
 
 <script lang="ts">
-  import { ref, defineComponent } from 'vue'
-  export default defineComponent({
-    name: 'HelloWorld',
-    props: {
-      msg: {
-        type: String,
-        required: true,
-      },
+  import { ColDef } from 'ag-grid-community'
+  import 'ag-grid-community/styles/ag-grid.css'
+  import 'ag-grid-community/styles/ag-theme-alpine.css'
+  import 'ag-grid-enterprise'
+  import { AgGridVue } from 'ag-grid-vue3'
+
+  export default {
+    name: 'App',
+    components: {
+      AgGridVue,
     },
-    setup: () => {
-      const count = ref(0)
-      return { count }
+    setup() {
+      const colDefs: ColDef[] = [
+        { headerName: 'Make', field: 'make' },
+        { headerName: 'Model', field: 'model' },
+        { headerName: 'Price', field: 'price' },
+      ]
+
+      return {
+        columnDefs: colDefs,
+        rowData: [
+          { make: 'Toyota', model: 'Celica', price: 35000 },
+          { make: 'Ford', model: 'Mondeo', price: 32000 },
+          { make: 'Porsche', model: 'Boxster', price: 72000 },
+        ],
+      }
     },
-  })
+  }
 </script>
 
 <style scoped>
